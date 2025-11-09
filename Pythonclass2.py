84674784fe99e15abe13250c542794453f3d6a5e
Aaryan0205 <ayuchamp2011@gmail.com>
2025-11-09 17:07:43 +0000

    Add files via upload

---
+++ Pythonclass2.py
@@ -0,0 +1,15 @@
+# # print("Hello world")
+# # # I am Aaryan and this is my first program
+# # x=5
+# # Name="Aaryan"
+# # print(x)
+# # a=input("enter the number ")
+# age=input("Enter your age")
+# name=input("Enter your name")
+# # print(a)
+# # print(age)
+# # print(name)
+# print("Hello",name,age) 
+# print("hello" "\n",name,age)
+import keyword
+print(keyword.kwlist)
