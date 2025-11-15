83c794915073730a89dff208d416bbd974e26c95
Aaryan0205 <ayuchamp2011@gmail.com>
2025-11-15 16:47:26 +0000

    Add files via upload

---
+++ Data type class.py
@@ -0,0 +1,14 @@
+a=45
+print("data type of a",type(a))
+x="Aaryan"
+print("data type of x",type (x))
+y=10.10
+print("data type of y",type(y))
+z=True
+print("data type of z",type(z))
+y=int(y)
+print("data type of y",type(y),y)
+name=str(input("enter your name"))
+r=name[::-1]
+name=r
+print("value of name",name)
