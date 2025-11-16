4f3951b90ee38fcecddee4549852f7a48577308f
Aaryan0205 <ayuchamp2011@gmail.com>
2025-11-16 18:54:24 +0000

    Add files via upload

---
+++ Python class 4(math).py
@@ -0,0 +1,24 @@
+# a=int(input("enter first num"))
+# b=int(input("enter second num"))
+# c=int(input("enter third num"))
+# d=int(input("enter fourth num"))
+# e=int(input("enter fifth num"))
+# sum=a+b+c+d+e
+# avg=sum / 5
+# print(avg)
+# amt=int(input("enter the amount"))
+# note_1=amt//100
+# note_2=(amt%100)//50
+# note_3=((amt%100)%50)//10
+# print("number of hundred rupee note",note_1)
+
+# print("number of fifty rupee note",note_2)
+
+# print("number of ten rupee note",note_3)
+x=int(input("enter the marks of your first subject"))
+y=int(input("enter the marks of your second subject"))
+z=int(input("enter the marks of your third subject"))
+m=int(input("enter the marks of your fourth subject"))
+s=int(input("enter the marks of your fifth subject"))
+amt=(x+y+z+m+s)/5
+print(amt)
