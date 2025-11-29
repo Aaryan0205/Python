2effa1e8b9fbcd71a4a19cfa03909aef03f5824d
Aaryan0205 <ayuchamp2011@gmail.com>
2025-11-29 16:40:19 +0000

    Add files via upload

---
+++ Python class on conditional statement.py
@@ -0,0 +1,22 @@
+# # # num=4
+
+# # # if num>0:
+# # #    print("positive")
+# # # else:
+# # #    print("negative")
+# # a=float(input("enter first number")) 
+# # b=float(input("enter second number"))
+# # if a>=b:
+# #     print("profit")
+# # else:
+# #     print("loss")
+# i=int(input("enter first number"))
+# if i>15:
+#     print("greater")
+# else:
+#  print("lower")
+z=int(input("enter first number"))
+if z%2==0:
+    print("even")
+else:
+    print("odd")
