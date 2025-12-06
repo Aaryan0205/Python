92c9fce5f696fa8102e06bf799e4172a2583b703
Aaryan0205 <ayuchamp2011@gmail.com>
2025-12-06 16:51:31 +0000

    Add files via upload

---
+++ python class 7.py
@@ -0,0 +1,17 @@
+# a=[2,4,5,6,9]
+# b=[1,2,3,4,5]
+# print(a is b)
+# print(b is not a)
+a=93
+b=64
+c=35
+d=99
+e=76
+AVG=((a+b+c+d+e)/5)
+print((a+b+c+d+e)/5)
+if AVG>90 or AVG==90:
+    print("Grade A")
+elif AVG>80 or AVG==80:
+    print("grade B")
+else:
+    print("grade C")
