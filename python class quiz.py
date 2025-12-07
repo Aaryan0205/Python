f4fc2be187792816a5fad12eacf1e36f9cd6b280
Aaryan0205 <ayuchamp2011@gmail.com>
2025-12-07 15:14:35 +0000

    Add files via upload

---
+++ python class quiz.py
@@ -0,0 +1,46 @@
+# a=4
+# b=45
+# c=18
+# d=14
+# e=2
+
+# sum=((a+b)*c)/d
+# print(sum)
+
+# name= "Aaryan"
+# Age= 14
+# if (name == "Aaryan" or name == "Bhaumik") and Age>= 20:
+#     print("eligible")
+# else:
+#     print("not eligible")
+
+# a=44
+# b=2
+
+# if a%b==0:
+#     print("yes")
+# else:
+#     print("no")
+
+# a=2
+# b=4
+# c=14
+# d=45
+# e=18
+
+# Mean=(a+b+c+d+e)/5
+# print(Mean)
+
+x=23
+y=28
+z=29
+AVG=(x+y+z)/3
+
+if AVG>x and  AVG>y and AVG>z:
+    print("Highest")
+
+elif AVG>x and AVG>y:
+    print("lower")
+else:
+    print("Invalid")
+    
