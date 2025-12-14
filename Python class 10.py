a7cc2d5823043cfbf0b803bcd964c92c5fcef8ce
Aaryan0205 <ayuchamp2011@gmail.com>
2025-12-14 16:52:05 +0000

    Add files via upload

---
+++ Python class 10.py
@@ -0,0 +1,16 @@
+# a=int(input("Enter your number"))
+# sum=0
+# for i in range(1,a+1):
+#     sum=sum+i
+#     print(sum)
+
+# a=input("Enter your own string")
+# b=('')
+# for i in a:
+#  b=i+b
+# print(a)
+# print(b)
+
+a=int(input("Enter your number"))
+for i in range(a,0,-1):
+    print(i)
