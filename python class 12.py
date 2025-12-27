0bc4014fa01f9225f6620e82a1759deb84374985
Aaryan0205 <ayuchamp2011@gmail.com>
2025-12-27 17:35:35 +0000

    Add files via upload

---
+++ python class 12.py
@@ -0,0 +1,19 @@
+# a=input("enter your name")
+# b=input("enter the letter")
+# i=0
+# count=0
+# while (i<len(a)):
+#     if(a[i]==b):
+#         count=count+1
+#     i=i+1
+# print(count)
+
+a=int(input("enter your number"))
+for i in  range(1,a+1):
+    if i>1:
+        for j in range(2,i):
+            if(i%j)==0:
+                break
+        else:
+            print(i)
+            
---
+++ python class 13.py
@@ -0,0 +1,98 @@
+# n=int(input("enter the number"))
+# for i in range (n):
+#     for j in range (i+1):
+#         print ("*",end="")
+#     print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n=int(input("Enter your number"))
+# a=1
+# for i in range(n):
+#     for j in range(i+1):
+#         print(a,end="")
+#         a+=1
+#     print()
+
+
+
+rowSize = int(input("enter the number of rows: "))
+if rowSize%2==0:
+  halfDiamRow = int(rowSize/2)
+else:
+  halfDiamRow = int(rowSize/2)+1
+space = halfDiamRow-1
+
+for i in range(1, halfDiamRow+1):
+  for j in range(1, space+1): 
+    print(end=" ")
+  space = space-1
+  num = 1
+  for j in range(2*i-1):
+    print(end=str(num))
+ 
+    num = num+1
+  print()
+space = 1
+
+for i in range(1, halfDiamRow):
+  for j in range(1, space+1): 
+    print(end=" ")
+  space = space+1
+  num = 1
+  for j in range(1, 2*(halfDiamRow-i)):
+    print(end=str(num)) 
+ 
+    num = num+1
+  print()
