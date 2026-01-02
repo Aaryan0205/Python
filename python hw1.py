89566ac5bfd40e035c930757d5dbdf5d25f49941
Aaryan0205 <ayuchamp2011@gmail.com>
2026-01-02 18:04:06 +0000

    Add files via upload

---
+++ python hw1.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.Screen().bgcolor("white")
+turtle.Screen().setup(550,750)
+Square=turtle.Turtle()
+num_side=4
+num_lenght=90
+angle=360.0/num_side
+Square=turtle.Turtle()
+Square.right(90)
+Square.forward(90)
+Square.right(90)
+Square.forward(90)
+Square.right(90)
+Square.forward(90)
+Square.right(90)
+Square.forward(90)
+turtle.done()
